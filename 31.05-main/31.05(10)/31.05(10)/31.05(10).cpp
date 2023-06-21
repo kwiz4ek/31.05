89543b93dbb864ac0998228d1d45e12e71f566bc
kwiz4ek <127256082+kwiz4ek@users.noreply.github.com>
2023-06-22 00:47:48 +0300

    Add files via upload

---
+++ 31.05-main/31.05(10)/31.05(10)/31.05(10).cpp
@@ -0,0 +1,104 @@
+﻿#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int score = 0;
+    int answer;
+
+    cout << "Ласкаво просимо до гри \"О, щасливчик!\"" << endl;
+    cout << "Відповідайте на наступні питання:" << endl;
+
+    cout << endl << "1. Скільки планет в Сонячній системі?" << endl;
+    cout << "   а) 5" << endl;
+    cout << "   б) 8" << endl;
+    cout << "   в) 10" << endl;
+    cout << "   Ваша відповідь: ";
+    cin >> answer;
+
+    if (answer == 8) {
+        cout << "   Правильно! +1 бал." << endl;
+        score++;
+    }
+    else {
+        cout << "   Неправильно. Гра завершена." << endl;
+        return 0;
+    }
+
+    cout << endl << "2. Яка найбільша планета в Сонячній системі?" << endl;
+    cout << "   а) Земля" << endl;
+    cout << "   б) Юпітер" << endl;
+    cout << "   в) Венера" << endl;
+    cout << "   Ваша відповідь: ";
+    cin >> answer;
+
+    if (answer == 'б') {
+        cout << "   Правильно! +1 бал." << endl;
+        score++;
+    }
+    else {
+        cout << "   Неправильно. Гра завершена." << endl;
+        return 0;
+    }
+
+    cout << endl << "3. Скільки континентів на Землі?" << endl;
+    cout << "   а) 4" << endl;
+    cout << "   б) 6" << endl;
+    cout << "   в) 7" << endl;
+    cout << "   Ваша відповідь: ";
+    cin >> answer;
+
+    if (answer == 'в') {
+        cout << "   Правильно! +1 бал." << endl;
+        score++;
+    }
+    else {
+        cout << "   Неправильно. Гра завершена." << endl;
+        return 0;
+    }
+
+    cout << endl << "4. Який хімічний елемент позначається символом \"H\"?" << endl;
+    cout << "   а) Гелій" << endl;
+    cout << "   б) Гідроген" << endl;
+    cout << "   в) Германій" << endl;
+    cout << "   Ваша відповідь: ";
+    cin >> answer;
+
+    if (answer == 'б') {
+        cout << "   Правильно! +1 бал." << endl;
+        score++;
+    }
+    else {
+        cout << "   Неправильно. Гра завершена." << endl;
+        return 0;
+    }
+
+    cout << endl << "5. Який рік зараз?" << endl;
+    cout << "   а) 2021" << endl;
+    cout << "   б) 2022" << endl;
+    cout << "   в) 2023" << endl;
+    cout << "   Ваша відповідь: ";
+    cin >> answer;
+
+    if (answer == 'в') {
+        cout << "   Правильно! +1 бал." << endl;
+        score++;
+    }
+    else {
+        cout << "   Неправильно. Гра завершена." << endl;
+        return 0;
+    }
+
+    cout << endl << "Ви набрали " << score << " бали." << endl;
+    if (score == 5) {
+        cout << "Вітаємо! Ви перемогли!" << endl;
+    }
+    else {
+        cout << "Спробуйте ще раз!" << endl;
+    }
+
+    return 0;
+}
---
+++ 31.05-main/31.05(3)/31.05(3)/31.05(3).cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int dayNumber;
+
+    cout << "Введіть день тижня (1-7): ";
+    cin >> dayNumber;
+
+    if (dayNumber == 1) {
+        cout << "Понеділоок" << endl;
+    }
+    else if (dayNumber == 2) {
+        cout << "Вівторок" << endl;
+    }
+    else if (dayNumber == 3) {
+        cout << "Середа" << endl;
+    }
+    else if (dayNumber == 4) {
+        cout << "Четвер" << endl;
+    }
+    else if (dayNumber == 5) {
+        cout << "П'ятница" << endl;
+    }
+    else if (dayNumber == 6) {
+        cout << "Субота" << endl;
+    }
+    else if (dayNumber == 7) {
+        cout << "Неділя" << endl;
+    }
+    else {
+        cout << "Некоректний день тижня!" << endl;
+    }
+
+    switch (dayNumber) {
+    case 1:
+        cout << "Понеділок" << endl;
+        break;
+    case 2:
+        cout << "Вівторок" << endl;
+        break;
+    case 3:
+        cout << "Середа" << endl;
+        break;
+    case 4:
+        cout << "Четвер" << endl;
+        break;
+    case 5:
+        cout << "П'ятница" << endl;
+        break;
+    case 6:
+        cout << "Субота" << endl;
+        break;
+    case 7:
+        cout << "Неділя" << endl;
+        break;
+    default:
+        cout << "Некоректний день тижня!" << endl;
+        break;
+    }
+
+    return 0;
+}
